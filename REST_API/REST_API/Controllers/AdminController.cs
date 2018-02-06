8963882cfcc86686b9e6a123d5d758eb7efbbe38
Matouš Kulhan <KulhanMatous@sssvt.cz>
2018-02-06 23:36:42 +0100

    Entity Framework

---
+++ REST_API/REST_API/Controllers/AdminController.cs
@@ -0,0 +1,25 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace REST_API.Controllers
+{
+    public class AdminController : ApiController
+    {
+        // GET api/admin
+        public string GetAll()
+        {
+            return JsonConvert.SerializeObject(new string[] { "value1", "value2" });
+        }
+
+        [Route("api/admin/{token}")]
+        public string Get(string token)
+        {
+            return token;
+        }
+    }
+}
