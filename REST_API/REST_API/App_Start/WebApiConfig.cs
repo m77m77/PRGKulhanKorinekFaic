218b7072ef78b31bd67d26987a5374e1a3c97485
Matouš Kulhan <KulhanMatous@sssvt.cz>
2018-02-06 23:58:46 +0100

    Config

--- REST_API/REST_API/App_Start/WebApiConfig.cs
+++ REST_API/REST_API/App_Start/WebApiConfig.cs
@@ -10,7 +10,7 @@ namespace REST_API
         public static void Register(HttpConfiguration config)
         {
             // Služby a konfigurace rozhraní Web API
-
+            config.Formatters.JsonFormatter.SupportedMediaTypes.Add(new System.Net.Http.Headers.MediaTypeHeaderValue("text/html"));
             // Trasy rozhraní Web API
             config.MapHttpAttributeRoutes();
 
--- REST_API/REST_API/Controllers/AdminController.cs
+++ REST_API/REST_API/Controllers/AdminController.cs
@@ -1,4 +1,5 @@
 ﻿using Newtonsoft.Json;
+using REST_API.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
