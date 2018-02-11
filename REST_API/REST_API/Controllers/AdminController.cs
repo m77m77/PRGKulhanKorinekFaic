a59dea6f25f005676643e357d4c6c6ed24351b4d
Tomáš Kořínek - připojení k database, metoda Get() - ValuesController <KorinekTomas@sssvt.cz>
2018-02-11 16:26:01 +0100

    Metoda Get() - uprava, Post() - skoro funguje

--- REST_API/REST_API/Controllers/AdminController.cs
+++ REST_API/REST_API/Controllers/AdminController.cs
@@ -25,12 +25,12 @@ namespace REST_API.Controllers
             if (t == null)
             {
                 //token není v databázi  
-                return new Response(null,"TokenNotFound",null,null);
+                return new Response("ERROR","TokenNotFound",null,null);
             }
             if (!t.IsAdmin)
             {
                 //token nepatří adminovi  
-                return new Response(null, "TokenIsNotMatched", null, null);
+                return new Response("ERROR", "TokenIsNotMatched", null, null);
             }
 
             MySqlCommand Query = Connection.CreateCommand();
@@ -49,21 +49,73 @@ namespace REST_API.Controllers
             try
             {
                 Connection.Open();
+                MySqlDataReader Reader = Query.ExecuteReader();
+
+                while (Reader.Read())
+                {
+                    data.ListSettings.Add(JsonConvert.DeserializeObject<Settings>(Reader["settings"].ToString()));
+                }
+                Reader.Close();
             }
             catch (MySql.Data.MySqlClient.MySqlException ex)
             {
-                
+                r = new Response("ERROR", "ConnectionWithDatabaseProblemm", null, null);
             }
-
-            MySqlDataReader Reader = Query.ExecuteReader();
-
-            while (Reader.Read())
-            {
-                data.ListSettings.Add(JsonConvert.DeserializeObject<Settings>(Reader["settings"].ToString()));
-            }
-
-            Reader.Close();
             Connection.Close();
+
+            if (r.Status == null)
+                r.Status = "OK";
+
+            return r;
+        }
+
+
+        [Route("api/admin/{token}")]
+        public Response Post(string token, [FromBody]Settings value)
+        {
+            MySqlConnection Connection = WebApiConfig.Connection();
+
+            Token t = Token.Exists(token);
+            if (t == null)
+            {
+                //token není v databázi  
+                return new Response("ERROR", "TokenNotFound", null, null);
+            }
+            if (!t.IsAdmin)
+            {
+                //token nepatří adminovi  
+                return new Response("ERROR", "TokenIsNotMatched", null, null);
+            }
+
+            MySqlCommand Query = Connection.CreateCommand();
+
+            Query.CommandText = "INSERT INTO `3b2_kulhanmatous_db2`.`daemons` (`settings`) VALUES (@value);";
+
+            Query.Parameters.AddWithValue("@value", value);
+
+            Response r = new Response();
+
+            ListSettingsData data = new ListSettingsData();
+            data.ListSettings = new List<Settings>();
+            r.Data = data;
+
+
+            try
+            {
+                Connection.Open();
+                Query.ExecuteNonQuery();
+
+                data.ListSettings.Add(JsonConvert.DeserializeObject<Settings>(value.ToString()));
+
+            }
+            catch (MySql.Data.MySqlClient.MySqlException ex)
+            {
+                r = new Response("ERROR", "ConnectionWithDatabaseProblemm", null, null);
+            }
+            Connection.Close();
+
+            if (r.Status == null)
+                r.Status = "OK";
 
             return r;
         }
