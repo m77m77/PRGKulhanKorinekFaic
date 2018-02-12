c7e6063ae6855ac1c86f6813b692514989418726
Matouš Kulhan <KulhanMatous@sssvt.cz>
2018-02-12 20:12:30 +0100

    Settings models

---
+++ REST_API/REST_API/Models/Settings/BackupTime.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace REST_API.Models.Settings
+{
+    public class BackupTime
+    {
+        public string Type { get; set; }
+        public TimeSpan Time { get; set; }
+        public int DayNumber { get; set; }
+    }
+}
