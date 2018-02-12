0b4cbdc47e0c047891732d36c71e3bf3dfc906fd
Tomáš Kořínek - připojení k database, metoda Get() - ValuesController <KorinekTomas@sssvt.cz>
2018-02-12 19:45:51 +0100

    AdminController - Post() - systemSettings

---
+++ REST_API/REST_API/Models/Settings/SystemSettings.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace REST_API.Models.Settings
+{
+    public class SystemSettings
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Value { get; set; }
+    }
+}
