79190eb800e705ae3cbbed013097dce44a9d4614
Tomáš Kořínek - připojení k database, metoda Get() - ValuesController <KorinekTomas@sssvt.cz>
2018-02-15 13:39:51 +0100

    DaemonController - Get()

---
+++ REST_API/REST_API/Controllers/DaemonController.cs
@@ -0,0 +1,70 @@
+﻿using MySql.Data.MySqlClient;
+using Newtonsoft.Json;
+using REST_API.CommunicationClasses;
+using REST_API.Models;
+using REST_API.Models.Settings;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace REST_API.Controllers
+{
+    public class DaemonController : ApiController
+    {
+        [Route("api/daemon/id/{token}")]
+        public Response Get(string token,int id)
+        {
+            MySqlConnection Connection = WebApiConfig.Connection();
+
+            Token t = Token.Exists(token);
+            if (t == null)
+            {
+                //token není v databázi  
+                return new Response("ERROR", "TokenNotFound", null, null);
+            }
+            if (!t.IsAdmin)
+            {
+                //token nepatří adminovi  
+                return new Response("ERROR", "TokenIsNotMatched", null, null);
+            }
+
+            MySqlCommand Query = Connection.CreateCommand();
+
+            Query.CommandText = "SELECT settings FROM daemons WHERE @id = id";
+
+            Query.Parameters.AddWithValue("@id", id);
+
+            Response r = new Response();
+
+            ListSettingsData data = new ListSettingsData();
+            data.ListSettings = new List<Settings>();
+            r.Data = data;
+
+            try
+            {
+                Connection.Open();
+                MySqlDataReader Reader = Query.ExecuteReader();
+
+                while (Reader.Read())
+                {
+                    data.ListSettings.Add(JsonConvert.DeserializeObject<Settings>(Reader["settings"].ToString(), new JsonSerializerSettings { TypeNameHandling = TypeNameHandling.Auto }));
+                    data.ListSettings[0].DaemonID = id;
+                }
+                Reader.Close();
+            }
+            catch (MySql.Data.MySqlClient.MySqlException ex)
+            {
+                r = new Response("ERROR", "ConnectionWithDatabaseProblem", null, null);
+            }
+            Connection.Close();
+
+            if (r.Status == null)
+                r.Status = "OK";
+
+            return r;
+        }
+    }
+}
