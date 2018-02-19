8b2796eb4c8ce4254996e62edcb7c9a69232767a
Tomáš Kořínek - připojení k database, metoda Get() - ValuesController <KorinekTomas@sssvt.cz>
2018-02-19 20:41:02 +0100

    EmailController

---
+++ REST_API/REST_API/Models/EmailSettings/EmailSettings.cs
@@ -0,0 +1,18 @@
+﻿using REST_API.CommunicationClasses;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace REST_API.Models.EmailSettings
+{
+    public class EmailSettings : IData
+    {
+        public string EmailAddress { get; set; }
+        public string ServerName { get; set; }
+        public int Port { get; set; }
+        public string From { get; set; }
+        public bool SslTls { get; set; }
+        public DateTime When { get; set; }
+    }
+}
