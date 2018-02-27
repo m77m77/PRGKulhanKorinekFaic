365b0efe9883ef49ae88fbf7d2a0d8bf0914ff0b
Matouš Kulhan <kulhanmatous@sssvt.cz>
2018-02-27 12:34:00 +0100

    BackupStatus

---
+++ REST_API/REST_API/Models/BackupStatus/BackupStatus.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using REST_API.CommunicationClasses;
+
+namespace REST_API.Models.BackupStatus
+{
+    public class BackupStatus : IData
+    {
+        public string Status { get; set; }
+        public string FailMessage { get; set; }
+        public List<BackupError> Errors { get; set; }
+    }
+}
