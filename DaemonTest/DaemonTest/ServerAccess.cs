23b10dd1699b441363e9944ca9330d84f5b434a1
Matouš Kulhan <KulhanMatous@sssvt.cz>
2018-03-05 22:33:11 +0100

    ServerAccess

---
+++ DaemonTest/DaemonTest/ServerAccess.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DaemonTest
+{
+    public static class ServerAccess
+    {
+        private static string serverAddress;
+        private static string token;
+
+        
+    }
+}
