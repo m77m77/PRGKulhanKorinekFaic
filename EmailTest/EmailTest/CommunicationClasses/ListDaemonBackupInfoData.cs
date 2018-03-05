01b34a757c459cf822ec410b3329d29d9392704f
korinektomas <korinektomas@sssvt.cz>
2018-03-05 09:40:14 +0100

    Email

--- EmailTest/EmailTest/CommunicationClasses/ListDaemonBackupInfoData.cs
+++ EmailTest/EmailTest/CommunicationClasses/ListDaemonBackupInfoData.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using EmailTest.Models.BackupInfo;
 
 namespace EmailTest.CommunicationClasses
 {
-    class ListDaemonBackupInfoData
+    public class ListDaemonBackupInfoData : IData
     {
+        public List<BackupStatus> ListDaemonBackupInfo { get; set; }
     }
 }
