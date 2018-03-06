97f843429eded09d80888134626616ce09e6cd15
Tomáš Kořínek - připojení k database, metoda Get() - ValuesController <korinektomas@sssvt.cz>
2018-03-06 19:23:25 +0100

    Email

---
+++ EmailTest/EmailTest/Models/BackupInfo/BackupError.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace EmailTest.Models.BackupInfo
+{
+    public class BackupError
+    {
+        public string Path { get; set; }
+        public string Message { get; set; }
+    }
+}
---
+++ EmailTest/EmailTest/Models/BackupInfo/BackupStatus.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmailTest.Models.BackupInfo
+{
+    public class BackupStatus : IData
+    {
+        public string Status { get; set; }
+        public DateTime TimeOfBackup { get; set; }
+        public string FailMessage { get; set; }
+        public List<BackupError> Errors { get; set; }
+    }
+}
