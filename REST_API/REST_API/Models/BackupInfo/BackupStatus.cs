a392913f907faeeb82ecf12f65541554d69b8fa3
Matouš Kulhan <KulhanMatous@sssvt.cz>
2018-03-08 05:49:32 +0100

    BackupInfo

---
+++ REST_API/REST_API/Models/BackupInfo/BackupStatus.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using REST_API.CommunicationClasses;
+
+namespace REST_API.Models.BackupInfo
+{
+    public class BackupStatus : IData
+    {
+        public string Status { get; set; }
+	    public DateTime TimeOfBackup { get; set; }
+        public string BackupType { get; set; }
+        public string FailMessage { get; set; }
+        public List<BackupError> Errors { get; set; }
+        public Dictionary<string,DateTime> Files { get; set; }
+    }
+}
