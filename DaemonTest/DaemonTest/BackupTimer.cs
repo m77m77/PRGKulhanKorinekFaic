f471d66955655759d036a82f08d6aa68c1ea670c
Matouš Kulhan <KulhanMatous@sssvt.cz>
2018-03-20 06:26:26 +0100

    Daemon

---
+++ DaemonTest/DaemonTest/BackupTimer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DaemonTest
+{
+    public class BackupTimer
+    {
+        public void Start()
+        {
+
+        }
+
+        public void Tick()
+        {
+
+        }
+    }
+}
---
+++ REST_API/REST_API/Models/Settings/Daemon.cs
@@ -0,0 +1,15 @@
+﻿using REST_API.CommunicationClasses;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace REST_API.Models.Settings
+{
+    public class Daemon : IData
+    {
+        public int DaemonID { get; set; }
+        public string DaemonName { get; set; }
+        public List<Settings> Settings { get; set; }
+    }
+}
