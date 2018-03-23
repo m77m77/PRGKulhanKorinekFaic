6a7b00aa483274c868c7ad15cd4934580ff2c32f
Tomáš Kořínek - připojení k database, metoda Get() - ValuesController <korinektomas@sssvt.cz>
2018-03-23 23:10:17 +0100

    Angular - login

--- WebAdminAppAngular5/WebAdminAppAngular5/ClientApp/app/components/login/login.component.ts
+++ WebAdminAppAngular5/WebAdminAppAngular5/ClientApp/app/components/login/login.component.ts
@@ -2,6 +2,7 @@
 import { Http, Headers, Response, URLSearchParams } from '@angular/http';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes, Router, ActivatedRoute } from '@angular/router';
+import 'rxjs/add/operator/toPromise';
 
 @Component({
     selector: 'login',
@@ -13,35 +14,52 @@ import { RouterModule, Routes, Router, ActivatedRoute } from '@angular/router';
 
 export class LoginComponent {
 
-    public constructor(private http: Http, private router: Router, private route: ActivatedRoute) {
+    public constructor(private http: Http, private router: Router, private route: ActivatedRoute, ) {
+        if (localStorage.getItem('token') != null) {
+            this.router.navigate(['../home'], { relativeTo: this.route })
+        }
+            
+        if (typeof window !== 'undefined') {
+            if (sessionStorage.getItem('token') != null)
+                this.router.navigate(['../home'], { relativeTo: this.route })
+        }
     }
 
     public login() {
         try {
+            var username = (<HTMLInputElement>document.getElementById('username')).value;
+            var password = (<HTMLInputElement>document.getElementById('password')).value
+
             var headers = new Headers();
-            headers.append('Content-Type', 'application/x-www-form-urlencoded');
-            let urlSearchParams = new URLSearchParams();
-            urlSearchParams.append('Name', 'admin');
-            urlSearchParams.append('Password', 'password');
-            let body = urlSearchParams.toString()
+            headers.append('Content-Type', 'application/json');
 
-            return this.http.post('http://localhost:51404/api/newtoken/admin', body, { headers: headers })
-                .lift((response: Response) => {
-                    // login successful if user.status = success in the response
+            return this.http.post('http://localhost:63058/api/newtoken/admin', { Name: 'admin', Password: 'password' }, { headers: headers }).toPromise()
+                .then((response: Response) => {
                     let user = response.json();
-                    console.log(user.status)
-                    if (user && "success" == user.status) {
-                        // store user details and jwt token in local storage to keep user logged in between page refreshes
-                        localStorage.setItem('currentUser', JSON.stringify(user.data));
+                    console.log(user.Status + " " + user.Error)
+                    if (user && "OK" == user.Status) {
+                        if (typeof window !== 'undefined') {
+                            sessionStorage.setItem('token', user.NewToken); // sessionStorage > localStorage => nemusíme dělat expiraci ručně
+                        }
 
-                        this.router.navigate([`../home`], { relativeTo: this.route })
+                        this.router.navigate(['../home'], { relativeTo: this.route })
+                    } else {
+                        console.log('Error: ' + user.Error)
                     }
-                });
-             }
+                })
+                .catch((msg: any) => console.log('Error: ' + msg.status + ' ' + msg.statusText))
+            }
         catch(e) {
 
-    }
-        
+        }
 
+    }
+    public Validation() {
+        var username = (<HTMLInputElement>document.getElementById('username')).value;
+        var password = (<HTMLInputElement>document.getElementById('password')).value
+
+        if (username != "" && password != "") {
+            this.login();
+        }
     }
 }
