15137174bf01d76c131a2370f4d8b847647c8e6a
Tomáš Kořínek - připojení k database, metoda Get() - ValuesController <korinektomas@sssvt.cz>
2018-03-23 20:52:43 +0100

    Angular - login

--- WebAdminAppAngular5/WebAdminAppAngular5/ClientApp/app/components/login/login.component.ts
+++ WebAdminAppAngular5/WebAdminAppAngular5/ClientApp/app/components/login/login.component.ts
@@ -1,4 +1,7 @@
 ﻿import { Component } from '@angular/core';
+import { Http, Headers, Response, URLSearchParams } from '@angular/http';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes, Router, ActivatedRoute } from '@angular/router';
 
 @Component({
     selector: 'login',
@@ -7,5 +10,38 @@
 
 
 })
+
 export class LoginComponent {
+
+    public constructor(private http: Http, private router: Router, private route: ActivatedRoute) {
+    }
+
+    public login() {
+        try {
+            var headers = new Headers();
+            headers.append('Content-Type', 'application/x-www-form-urlencoded');
+            let urlSearchParams = new URLSearchParams();
+            urlSearchParams.append('Name', 'admin');
+            urlSearchParams.append('Password', 'password');
+            let body = urlSearchParams.toString()
+
+            return this.http.post('http://localhost:51404/api/newtoken/admin', body, { headers: headers })
+                .lift((response: Response) => {
+                    // login successful if user.status = success in the response
+                    let user = response.json();
+                    console.log(user.status)
+                    if (user && "success" == user.status) {
+                        // store user details and jwt token in local storage to keep user logged in between page refreshes
+                        localStorage.setItem('currentUser', JSON.stringify(user.data));
+
+                        this.router.navigate([`../home`], { relativeTo: this.route })
+                    }
+                });
+             }
+        catch(e) {
+
+    }
+        
+
+    }
 }
