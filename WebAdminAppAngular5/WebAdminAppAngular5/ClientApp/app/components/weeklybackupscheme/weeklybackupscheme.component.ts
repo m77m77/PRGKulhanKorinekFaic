daf7902aa447399c10da0678d8b5ed5b044cba25
Tomáš Kořínek - připojení k database, metoda Get() - ValuesController <korinektomas@sssvt.cz>
2018-03-25 00:30:00 +0100

    Angular - weeklybackupscheme

--- WebAdminAppAngular5/WebAdminAppAngular5/ClientApp/app/components/weeklybackupscheme/weeklybackupscheme.component.ts
+++ WebAdminAppAngular5/WebAdminAppAngular5/ClientApp/app/components/weeklybackupscheme/weeklybackupscheme.component.ts
@@ -1,4 +1,4 @@
-﻿import { Component } from '@angular/core';
+﻿import { Component, ElementRef } from '@angular/core';
 
 @Component({
     selector: 'weeklybackupscheme',
@@ -8,26 +8,46 @@
 })
 
 export class WeeklybackupScheme {
-    constructor(private position: InsertPosition) { }
-
+    constructor(private position: InsertPosition, private elementRef: ElementRef) { }
+    
     public OpenPanel(button: string) {
         this.position = "afterbegin";
-        document.activeElement.insertAdjacentHTML(this.position, '<div class="panel panel-default" >'
-                                                               + '<div class="panel-body" ><button id="idbtnplus" (click)="btnAdd()">+</button>'
-                                                               + '<button id="idbtnminus" (click)="btnSubstract()">-</button>< /div>'
-                                                               + '< /div>')
+
+        var d1 = this.elementRef.nativeElement.querySelector('.one');
+        d1.insertAdjacentHTML(this.position, '<div class="panel panel-default" >'
+                                           + '<div class="panel-body" ><button id="idbtnplus" (click)="btnAdd()">+</button>'
+                                           + '<button id="idbtnminus" (click)="btnSubstract()">-</button>< /div>'
+                                           + '< /div>')
+
+
+        
+        //document.activeElement.insertAdjacentHTML(this.position, '<div class="panel panel-default" >'
+        //                                                       + '<div class="panel-body" ><button id="idbtnplus" (click)="btnAdd()">+</button>'
+        //                                                       + '<button id="idbtnminus" (click)="btnSubstract()">-</button>< /div>'
+        //                                                       + '< /div>')
 
     }
 
     public btnAdd() {
     var btn = document.getElementById("btnAddbackup")
     var btnplus = document.getElementById("idbtnplus")
+
+    var d1 = this.elementRef.nativeElement.querySelector('.two');
+    d1.insertAdjacentHTML(this.position, '<input type="text" name="scheme" list="exampleList">'
+                                        +'< datalist id= "exampleList" >'
+                                        +'<option value="FULL" >'
+                                        +'<option value="INCREMENTAL" >'
+                                        +'<option value="DIFFERENCIAL" >'
+                                        +'</datalist>')
     
 }
 
     public btnSubstract() {
     var btn = document.getElementById("btnAddbackup")
     var btnminus = document.getElementById("idbtnminus")
+
+    var d1 = this.elementRef.nativeElement.querySelector('.two');
+    d1.insertAdjacentHTML(this.position, '')
 
 }
 }
