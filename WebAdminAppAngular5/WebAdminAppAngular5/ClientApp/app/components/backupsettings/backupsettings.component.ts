7193d330ffd150e9b25c6db41ae9a112b0faf030
Matouš Kulhan <KulhanMatous@sssvt.cz>
2018-03-25 21:28:54 +0200

    BackupSettings

---
+++ WebAdminAppAngular5/WebAdminAppAngular5/ClientApp/app/components/backupsettings/backupsettings.component.ts
@@ -0,0 +1,15 @@
+﻿import { Component } from '@angular/core';
+import { Http, Headers, Response } from '@angular/http';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes, Router, ActivatedRoute } from '@angular/router';
+import 'rxjs/add/operator/toPromise';
+
+@Component({
+    selector: 'backupsettings',
+    templateUrl: './backupsettings.component.html',
+    styleUrls: ['./backupsettings.component.css']
+
+
+})
+export class BackupSettingsComponent {
+}
