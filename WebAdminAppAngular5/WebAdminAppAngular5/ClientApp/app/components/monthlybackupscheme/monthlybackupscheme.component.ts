22c4c0da2c3046ce996e3b7f2007c20000c42bcc
davidfaic <davidfaic@seznam.cz>
2018-03-26 19:11:19 +0200

    dailybackupscheme + css

--- WebAdminAppAngular5/WebAdminAppAngular5/ClientApp/app/components/monthlybackupscheme/monthlybackupscheme.component.ts
+++ WebAdminAppAngular5/WebAdminAppAngular5/ClientApp/app/components/monthlybackupscheme/monthlybackupscheme.component.ts
@@ -1,1 +1,11 @@
-﻿
+﻿import { Component } from '@angular/core';
+
+@Component({
+    selector: 'monthlybackupscheme',
+    templateUrl: './dailybackupscheme.component.html',
+    styleUrls: ['./dailybackupscheme.component.css']
+
+
+})
+export class MonthlybackupschemeComponent {
+}
