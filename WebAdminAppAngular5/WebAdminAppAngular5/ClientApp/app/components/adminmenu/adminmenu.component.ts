7708545b4f7cbff660277a223746aa03468bf74c
Tomas <Tomas@TOMAS-PC>
2018-04-03 12:31:09 +0200

    Angular - ngFor-adminmenu

--- WebAdminAppAngular5/WebAdminAppAngular5/ClientApp/app/components/adminmenu/adminmenu.component.ts
+++ WebAdminAppAngular5/WebAdminAppAngular5/ClientApp/app/components/adminmenu/adminmenu.component.ts
@@ -3,6 +3,7 @@ import { Http, Headers, Response } from '@angular/http';
 import { NgModule, ElementRef, Renderer2 } from '@angular/core';
 import { RouterModule, Routes, Router, ActivatedRoute } from '@angular/router';
 import 'rxjs/add/operator/toPromise';
+import { NgForOf } from '@angular/common';
 
 @Component({
     selector: 'adminmenu',
@@ -14,6 +15,8 @@ import 'rxjs/add/operator/toPromise';
 export class AdminMenuComponent {
     private elementRef: ElementRef
     private position: InsertPosition
+
+    admins: any;
 
     constructor(private renderer: Renderer2, private http: Http, private router: Router, private route: ActivatedRoute) {
         if (typeof window !== 'undefined') {
@@ -40,6 +43,8 @@ export class AdminMenuComponent {
         var htmlCode = '';
         var btnName = '';
 
+
+
         var AdminPost = sessionStorage.getItem('AdminPost');
         if (AdminPost != null) {
             var data = JSON.parse(AdminPost);
@@ -47,17 +52,20 @@ export class AdminMenuComponent {
         }
 
         var count = data.ListAdmin.length;
-
+        this.admins = [];
         for (var n = 0; n < count; n++) {
             btnName = data.ListAdmin[n].Name
-            htmlCode = htmlCode + '<p><button id="' + btnName + '" (click)="OpenAdminInfo("' + btnName + '")">' + btnName + '</button></p>';
+            //htmlCode = htmlCode + '<p><button id="' + btnName + '" (click)="OpenAdminInfo("' + btnName + '")">' + btnName + '</button></p>';
+            this.admins.push({
+                Name: btnName == btnName
+            });
         }
 
-        var div = document.createElement("div");
-        var d1 = (<HTMLInputElement>document.getElementById('code'))
-        div.className = "adminmenu";
-        div.innerHTML = htmlCode;
-        this.renderer.appendChild(d1,div);
+        //var div = document.createElement("div");
+        //var d1 = (<HTMLInputElement>document.getElementById('code'))
+        //div.className = "adminmenu";
+        //div.innerHTML = htmlCode;
+        //this.renderer.appendChild(d1,div);
     }
     public OpenAdminInfo(adminName: string) {
         sessionStorage.setItem('adminInfoName', adminName);
