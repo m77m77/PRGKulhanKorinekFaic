c44656351382dd241f9b6c3747842376088f75e7
Tomáš Kořínek - připojení k database, metoda Get() - ValuesController <korinektomas@sssvt.cz>
2018-04-09 21:41:26 +0200

    angular - settings add/delete

--- WebAdminAppAngular5/WebAdminAppAngular5/ClientApp/app/components/settings/settings.component.ts
+++ WebAdminAppAngular5/WebAdminAppAngular5/ClientApp/app/components/settings/settings.component.ts
@@ -15,4 +15,14 @@ export class SettingsComponent {
     constructor(private http: Http, private router: Router, private route: ActivatedRoute) {
         this.route.params.subscribe(params => { if (typeof (window) !== 'undefined') { sessionStorage.setItem('settingsID', params.settingsID); }});
     }
+    public DeleteSettings() {
+        var SettingsID = sessionStorage.getItem('settingsID');
+        if (SettingsID != 'default'){
+            this.http.delete('http://localhost:63058/api/newsettings/delete/' + sessionStorage.getItem('token') + '/' + SettingsID).toPromise()
+            .catch((msg: any) => { sessionStorage.removeItem('token'); this.router.navigate(['/login'], {}) })
+            window.location.reload();
+            console.log('deleted' + SettingsID)
+        }
+        
+    }
 }
