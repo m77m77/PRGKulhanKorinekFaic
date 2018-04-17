2af8aa97a8baf169a3ad688bfed1544efdb1b3c1
Matouš Kulhan <m77m77@seznam.cz>
2018-04-17 12:19:10 +0200

    Delete settings

--- WebAdminAppAngular5/WebAdminAppAngular5/ClientApp/app/components/settings/settings.component.ts
+++ WebAdminAppAngular5/WebAdminAppAngular5/ClientApp/app/components/settings/settings.component.ts
@@ -19,23 +19,4 @@ export class SettingsComponent{
     constructor(private http: Http, private router: Router, private route: ActivatedRoute) {
         this.route.params.subscribe(params => { if (typeof (window) !== 'undefined') { this.display = 'display:none'; sessionStorage.setItem('settingsID', params.settingsID); }});
     }
-
-    public DeleteSettings() {
-        var daemonsData = sessionStorage.getItem('daemonsData');
-        var daemonID = sessionStorage.getItem('daemonID');
-        var settingsrow = sessionStorage.getItem('settingsID');
-        var SettingsID;
-
-        if (daemonsData != null && daemonID != null && settingsrow != null) {
-            var data = JSON.parse(daemonsData);
-            var listDaemons = data.ListDaemons;
-            SettingsID = listDaemons[daemonID].Settings[settingsrow].SettingsID;
-        }
-
-        if (SettingsID != 'default'){
-            this.http.delete('http://localhost:63058/api/newsettings/delete/' + sessionStorage.getItem('token') + '/' + SettingsID).toPromise()
-            .catch((msg: any) => { sessionStorage.clear(); this.router.navigate(['/login'], {}) })
-            window.location.reload();
-        }
-    }
 }
