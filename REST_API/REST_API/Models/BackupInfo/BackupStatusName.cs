81287cdbb7eb24e06e536200ee06d4f8ad99ea99
Tomas <Tomas@TOMAS-PC>
2018-04-18 08:39:45 +0200

    Angular - backup status - name

---
+++ REST_API/REST_API/Models/BackupInfo/BackupStatusName.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using REST_API.CommunicationClasses;
+
+namespace REST_API.Models.BackupInfo
+{
+    public class BackupStatusName : IData
+    {
+        public BackupStatus backupStatus { get; set; }
+        public string Name { get; set; }
+    }
+}
