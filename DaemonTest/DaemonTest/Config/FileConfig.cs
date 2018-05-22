c439ca8a5439e1f4afba9543f540f753df9db47e
Tomas <Tomas@TOMAS-PC>
2018-05-22 11:56:20 +0200

    angular - enable

--- DaemonTest/DaemonTest/Config/FileConfig.cs
+++ DaemonTest/DaemonTest/Config/FileConfig.cs
@@ -16,7 +16,7 @@ namespace DaemonTest.Config
         {
             this.Server = "http://localhost:63058";
             this.Token = "";// "zZn4L8,WKTb6iEPonSEa5vP3dEsHQZmk";
-            this.InitializationToken = "lkbb3aIX0SwFTCDziyBVzNdOHL3ovr8g";
+            this.InitializationToken = "B--EADMsctjTDn8uwxjTQMwb587hp-nB"; // "lkbb3aIX0SwFTCDziyBVzNdOHL3ovr8g";
             return true;
         }
 
