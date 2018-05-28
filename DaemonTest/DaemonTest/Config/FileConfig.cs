1468c8d3ae625c26b799d13d6217af0547187d6b
Matouš Kulhan <KulhanMatous@sssvt.cz>
2018-05-28 21:08:05 +0200

    FileConfig

--- DaemonTest/DaemonTest/Config/FileConfig.cs
+++ DaemonTest/DaemonTest/Config/FileConfig.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using DaemonTest.Utilities;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Text;
 
 namespace DaemonTest.Config
@@ -12,17 +14,49 @@ namespace DaemonTest.Config
 
         public string InitializationToken { get; set; }
 
+        private string path;
+
+        public FileConfig()
+        {
+            this.path = Path.Combine(Environment.CurrentDirectory,"config.json");
+        }
+
         public bool Load()
         {
-            this.Server = "http://localhost:63058";
-            this.Token = "";// "zZn4L8,WKTb6iEPonSEa5vP3dEsHQZmk";
-            this.InitializationToken = "lL0KJqv3dv,TVHR7hwGgBzUGf6TkgNh4";
-            return true;
+            //this.Server = "http://localhost:63058";
+            //this.Token = "";// "zZn4L8,WKTb6iEPonSEa5vP3dEsHQZmk";
+            //this.InitializationToken = "M3d3S5z,hod1XEb5jLaqGOk3FrQvkkvb";
+
+            try
+            {
+                StreamReader reader = new StreamReader(this.path);
+
+                FileConfig config = JsonSerializationUtility.Deserialize<FileConfig>(reader.ReadToEnd());
+                reader.Close();
+
+                this.Server = config.Server;
+                this.Token = config.Token;
+                this.InitializationToken = config.InitializationToken;
+
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
         }
 
         public void Save()
         {
-            
+            try
+            {
+                StreamWriter writer = new StreamWriter(this.path);
+                writer.Write(JsonSerializationUtility.Serialize(this));
+                writer.Close();
+            }
+            catch (Exception)
+            {
+            }
         }
     }
 }
